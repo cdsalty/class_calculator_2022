f275c9b8b9b37c478cf7dbf8a336ade8af6861ba
cdsalty <soltiscd@gmail.com>
2022-03-12 12:25:26 -0500

    next step, seperate enteries and calculations

--- script.js
+++ script.js
@@ -1,38 +1,69 @@
 /*
 https://digitalcrafts.instructure.com/courses/190/pages/group-in-class-activity-calculator?module_item_id=27454
 */
-
 let numbers = document.querySelectorAll(".numbers div");
-// console.log(numbers);
 let operations = document.querySelectorAll(".operators div");
-// console.log(operations);
 let input = document.getElementById("input");
-console.log(input);
+let numbersToCalculate = [];
 
 let decimalFlag = false;
+let operandFlag = false;
+
+let arrayOfNumbers = [];
+let arrayOfOperations = [];
 
 for (let i = 0; i < numbers.length; i++) {
   // Add an event listener to all divs/button
   numbers[i].addEventListener("click", (e) => {
+    if (numbers[i].innerHTML != "C" && numbers[i].innerHTML != ".") {
+      arrayOfNumbers.push(numbers[i].innerHTML);
+      operandFlag = false;
+      console.log(arrayOfNumbers);
+      console.log(operandFlag);
+      input.innerHTML += e.target.innerHTML;
+    }
+
     if (e.target.innerHTML == "C") {
-      // console.log(e.target.innerHTML);
+      console.log(e.target.innerHTML);
       decimalFlag = false;
+      operandFlag = false;
       input.innerHTML = "";
-    } else {
-      if (!decimalFlag && e.target.innerHTML != ".") {
-        input.innerHTML += e.target.innerHTML;
-      } else if (!decimalFlag) {
-        decimalFlag = true;
-        input.innerHTML += e.target.innerHTML;
-      }
+      arrayOfNumbers = [];
+      arrayOfOperations = [];
+      console.log("Array of Operations", arrayOfOperations);
+      console.log("Array of Operations", arrayOfOperations);
+    }
+
+    if (!decimalFlag && e.target.innerHTML == ".") {
+      input.innerHTML += e.target.innerHTML;
+      decimalFlag = true;
+      console.log("Decimal?", decimalFlag);
+      arrayOfNumbers.push(e.target.innerHTML);
+    } else if (decimalFlag) {
+      console.log("Decimal pressed again");
+      console.log("decimal flag?", decimalFlag);
     }
   });
 }
 
 for (let i = 0; i < operations.length; i++) {
   operations[i].addEventListener("click", (e) => {
-    console.log(e);
+    // operandFlag = true;
+    if (operandFlag == false && input.innerHTML != "") {
+      decimalFlag = false;
+      combineArrayNumbers();
+      arrayOfOperations.push(operations[i].innerHTML);
+      operandFlag = true;
+      console.log("Array of Operations", arrayOfOperations); //same thing as console.log("Array of Operations", arrayOfOperations)
+      console.log("Operations?", operandFlag);
+    }
   });
 }
 
-console.log(numbers);
+function combineArrayNumbers() {
+  // get the numbers array;
+  // slice array and then combine
+  numbersToCalculate.push(arrayOfNumbers.join(""));
+  arrayOfNumbers = [];
+  console.log("Concat string: ", numbersToCalculate);
+}
